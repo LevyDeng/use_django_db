971a5e802cc9e48131dac270a4c095fbdd773a1e
root <ptrees@126.com>
2017-03-25 22:35:32 +0800

    finally complated

--- stats.py
+++ stats.py
@@ -30,6 +30,7 @@ logerror.addHandler(ferror)
 BASE_URL="http://300report.jumpw.com/"
 STARTID=67482286
 TIME_SLEEP=2
+FLAG=0
 
 class MatchStat():
     def stat_match(self,matchid):
@@ -43,10 +44,12 @@ class MatchStat():
                     datamsg=soup.find_all('span')[1].next_element.next_element.split()
                     match_info=MatchInfo(matchid=matchid)
                     match_info.info_type=datamsg[0].split(':')[1]
-                    if match_info.info_type==u'竞技场':        #战场，竞技场数据不同，偏移量为4
-                        FLAG=0
+                    print match_info.info_type
+                    if match_info.info_type=='战场':        #战场，竞技场数据格式不同，偏移量为4
+                        FLAG=-2
                     else:
-                        FLAG=-4
+                        FLAG=2
+                    print FLAG
                     match_info.info_date=datamsg[2].split(':')[1]
                     match_info.info_time=datamsg[3]
                     playtime=datamsg[4].split(":")[1]
@@ -60,67 +63,67 @@ class MatchStat():
                         try:
                             tds=[ t for t in trs[i].children ]
                         except IndexError:
-                            print i,matchid
+                            continue
+                            #print i,matchid
                         self.writeattr(match_info,'player%s_resault'%str(i),True if (trs[i].parent.previous_element.previous_element==u"胜利") else False)
                         if tds[3].a:
-                            self.writeattr(match_info,'player%s_name'%str(i),tds[3].a.string.split('(')[0],'char')
+                            self.writeattr(match_info,'player%s_name'%str(i),tds[3].a.string.split('(')[0])
                         try:
-                            self.writeattr(match_info,'player%s_hero'%str(i),tds[3].a.next_element.next_element.next_element.split('(')[0],'char')
+                            self.writeattr(match_info,'player%s_hero'%str(i),tds[3].a.next_element.next_element.next_element.split('(')[0])
                         except Exception,e:
-                            print i,matchid,e
+                            #print i,matchid,e
+                            loginfo.warning(e)
                         self.writeattr(match_info,'player%s_user_lv'%str(i),int(re.findall("lv\.(\d+)",str(tds[3]))[0]))
                         self.writeattr(match_info,'player%s_hero_lv'%str(i),int(re.findall("lv\.(\d+)",str(tds[3]))[1]))
                         kills=tds[5].string.split('/')
                         self.writeattr(match_info,'player%s_kills'%str(i),int(kills[0]))
                         self.writeattr(match_info,'player%s_dies'%str(i),int(kills[1]))
                         self.writeattr(match_info,'player%s_helps'%str(i),int(kills[1]))
-                        self.writeattr(match_info,'player%s_wins'%str(i),tds[7].string,'char')
+                        self.writeattr(match_info,'player%s_wins'%str(i),tds[7].string)
                         self.writeattr(match_info,'player%s_soldiers'%str(i),int(tds[11].string))
-                        if FLAG==0:
-                            self.writeattr(match_info,'player%s_golds'%str(i),tds[13].string,'int')
-                        if tds[17+FLAG].img:
-                            self.writeattr(match_info,'player%s_skill1'%str(i),tds[17+FLAG].img['title'],'char')
-                            self.writeattr(match_info,'player%s_skill2'%str(i),tds[17+FLAG].img.next_sibling['title'],'char')
-                        items=tds[19+FLAG].find_all('img')
+                        self.writeattr(match_info,'player%s_skill1'%str(i),tds[15+FLAG].img['title'])
+                        self.writeattr(match_info,'player%s_skill2'%str(i),tds[15+FLAG].img.next_sibling['title'])
+                        print tds[15+FLAG].img['title']
+                        items=tds[17+FLAG].find_all('img')
                         if len(items)!=0:
                             for j in range(1,len(items)+1):
-                                self.writeattr(match_info,'player%s_item%s'%(str(i),str(j)),items[j-1]['title'],'char')
-                        if FLAG==0:
-                            gain_golds_exps=tds[21].split('/')
-                            if len(gain_golds_exps) != 0:
-                                self.writeattr(match_info,'player%s_gain_golds'%str(i),int(gain_golds_exps[0]))
-                                self.writeattr(match_info,'player%s_gain_exp'%str(i),int(gain_golds_exps[1]))
-                        if FLAG==0:
+                                self.writeattr(match_info,'player%s_item%s'%(str(i),str(j)),items[j-1]['title'])
+                                print items[j-1]['title']
+                        if FLAG==2:
+                            self.writeattr(match_info,'player%s_golds'%str(i),tds[13].string)
+                            try:
+                                gain_golds_exps=tds[21].string.split('/')
+                                if len(gain_golds_exps) != 0:
+                                    self.writeattr(match_info,'player%s_gain_golds'%str(i),int(gain_golds_exps[0]))
+                                    self.writeattr(match_info,'player%s_gain_exp'%str(i),int(gain_golds_exps[1]))
+                            except TypeError:
+                                continue
                             self.writeattr(match_info,'player%s_jiecao'%str(i),int(tds[23].string))
                             self.writeattr(match_info,'player%s_win_p'%str(i),tds[25].string)
                         try:
                             match_info.save()
                             loginfo.info("成功存储比赛信息，matchid:%d"%matchid)
-                            connection.close()
+                            #connection.close()
                         except IOError,e:
                             logerror.error("\t\t"+str(e)+"存储详细信息失败,"+"matchid:%d"%matchid)
             else:
                 loginfo.warning("match:%d 未获取到比赛信息。"%matchid)
 
 
-        except Exception,e:
+        except IOError,e:
         #except Exception,e:
             logerror.warn(str(e)+"网络连接失败")
 
-    def writeattr(self,classname,key,value,type=None):
+    def writeattr(self,classname,key,value):
         try:
             if value:
-                if type=='char':
-                    value=unicode(value)
-                if type=='int':
-                    value=int(value)
                 setattr(classname,key,value)
         except IndexError:
             logerror.warning("/t/tmatchid:%(matchid)d %(key)s net set!")
 
     def run(self,num):
 
-        for i in range(int(num+1)):
+        for i in range(int(num)):
             t=threading.Thread(target=self.stat_match,args=(STARTID+i,))
             t.start()
             sleep(TIME_SLEEP)
